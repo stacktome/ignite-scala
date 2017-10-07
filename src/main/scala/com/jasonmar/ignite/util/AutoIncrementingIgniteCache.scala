77aa898fda5b3c4a393d226992a700a058dadcd6
Jason Mar <jasonmar@gmail.com>
2017-10-06 20:39:30 -0400

    v1.6.2 add AutoIncrementingIgniteCache

--- src/main/scala/com/jasonmar/ignite/util/AutoIncrementingIgniteCache.scala
+++ src/main/scala/com/jasonmar/ignite/util/AutoIncrementingIgniteCache.scala
@@ -4,7 +4,9 @@ import org.apache.ignite.{Ignite, IgniteAtomicSequence, IgniteCache}
 
 case class AutoIncrementingIgniteCache[V](ignite: Ignite, cache: IgniteCache[Long,V]) {
   protected val seq: IgniteAtomicSequence = ignite.atomicSequence(cache.getName + "AutoIncrId", 0L, true)
-  def put(v: V): Unit = {
-    cache.put(seq.incrementAndGet(), v)
+  def put(v: V): Long = {
+    val key = seq.incrementAndGet()
+    cache.put(key, v)
+    key
   }
 }
